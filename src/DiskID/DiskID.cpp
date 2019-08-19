74dd761c619ed6d640695627d059349d530cde04
caishi8860@163.com <caishi8860@163.com>
2019-08-19 17:50:27 +0800

    first submit

---
+++ src/DiskID/DiskID.cpp
@@ -0,0 +1,19 @@
+﻿// DiskID.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+
+#include "pch.h"
+#include "HardDriveSerialNumer.h"
+#include <iostream>
+
+int main()
+
+{
+	char SerialNumber[1024] = { 0 };
+	MasterHardDiskSerial mhds;
+	mhds.GetSerialNo(SerialNumber);
+	std::cout << "SN:\t" << SerialNumber << std::endl;
+	system("pause");
+	return 0;
+
+}
